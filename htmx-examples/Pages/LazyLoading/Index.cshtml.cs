750489b5486c960eb6189bdb2a6b36f906717ad3
Chris Woody Woodruff <cwoodruff@live.com>
2026-01-05 18:02:27 -0500

    Lazy Loading demo and more content

--- htmx-examples/Pages/LazyLoading/Index.cshtml.cs
+++ htmx-examples/Pages/LazyLoading/Index.cshtml.cs
@@ -13,8 +13,17 @@ public class IndexModel : PageModel
     {
     }
 
-    public IActionResult OnGetGraph()
+    public async Task<IActionResult> OnGetSalesChart()
     {
-        return Content($"<img alt=\"Tokyo Climate\" src=\"https://htmx.org/img/tokyo.png\">");
+        // Simulate a slow database or API call
+        await Task.Delay(2000);
+        return Partial("_SalesChart");
+    }
+
+    public async Task<IActionResult> OnGetRecentActivity()
+    {
+        // Simulate another slow call
+        await Task.Delay(1000);
+        return Partial("_RecentActivity");
     }
 }
